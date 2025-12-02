be7e8350bbb7001d07ed0ec261907ff19c5863ae
Foulla <samankassoufoulla@gmail.com>
2025-12-02 18:01:47 +0100

    feat(navigation): add bottom tab navigation with five sections
    Replace single-page navigation with TabBar containing:
    - Accueil (Property List)
    - Favoris (Favorites)
    - Menu
    - Événements (Events)
    - Compte (Account)
    
    Register HomePage and PropertyListPage routes.

--- AppShell.xaml.cs
+++ AppShell.xaml.cs
@@ -8,6 +8,8 @@
 
             // Register routes for navigation
             Routing.RegisterRoute(nameof(Views.LoginPage), typeof(Views.LoginPage));
+            Routing.RegisterRoute(nameof(Views.HomePage), typeof(Views.HomePage));
+            Routing.RegisterRoute(nameof(Views.PropertyListPage), typeof(Views.PropertyListPage));
         }
     }
 }
